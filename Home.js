aa5091c08fa79c944c287aa285bfba27637b3a77
Nabarun Gathiri <you@example.com>
2022-07-06 22:29:37 +0530

    added to local

---
+++ Home.js
@@ -0,0 +1,101 @@
+const navSlide=()=>{
+    const burger=document.querySelector('.burger');
+    const nav=document.querySelector('.nav-links');
+    const navLinks=document.querySelectorAll('.nav-links li');
+
+    burger.addEventListener('click',()=>{
+    // Toggle navs
+        nav.classList.toggle('nav-active');
+
+        if(nav.style.animation){
+            nav.style.animation='';
+        }else{
+            nav.style.animation=`navcolor 0.5s ease forwards `;
+        }
+
+    // Animate Links
+    
+        navLinks.forEach((link,index)=>{
+            if(link.style.animation){
+             link.style.animation=''
+            }else{
+             link.style.animation=`navLinkFade 0.5s ease forwards ${index/7 + 0.5}s`;
+            }
+         })
+     // burger animation 
+         burger.classList.toggle('toggle');
+    });
+
+    window.addEventListener('resize',()=>{
+        if(window.innerWidth>960){
+            nav.style.animation='';
+        }
+    })
+    
+}
+
+navSlide();
+
+
+// SLider part
+const slides=document.querySelectorAll('.slide');
+
+let currSlide=0;
+let maxSlide=slides.length;
+
+const slider=document.querySelector('.slider');
+// slider.style.transform='scale(0.8)';
+// slider.style.overflow='visible';
+
+slides.forEach((s,i)=> s.style.transform=
+    `translateX(${100*i}%)`);
+
+slider.addEventListener('click',function(){
+    if(currSlide===maxSlide-1){
+        currSlide=0;
+    }else{
+        currSlide++;
+    }
+
+    slides.forEach((s,i)=> s.style.transform=
+    `translateX(${100*(i-currSlide)}%)`);
+});   
+
+
+
+
+
+// Scroll on click of contact us
+
+const here=document.getElementById('clingy');
+const tohere=document.getElementById('scroll');
+
+here.addEventListener('click',function(e){
+    e.preventDefault();
+    tohere.scrollIntoView({behavior:"smooth"});
+})
+
+
+
+
+
+// reveal elements on scroll
+const allSections=document.querySelectorAll('.section');
+const revealSection =function(entries,observer){
+    const [entry]=entries;
+
+    if(!entry.isIntersecting) return;
+    entry.target.classList.remove('section--hidden');
+
+    observer.unobserve(entry.target);
+
+};
+const sectionObserver=new IntersectionObserver(revealSection,{
+    root:null,
+    threshold:0.15,
+});
+
+allSections.forEach(function(section){
+    sectionObserver.observe(section);
+    section.classList.add('section--hidden');
+});
